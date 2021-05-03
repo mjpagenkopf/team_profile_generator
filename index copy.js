bc406fdd6fe53aa048e1b479af7055c309a304af
mjpagenkopf <78058404+mjpagenkopf@users.noreply.github.com>
2021-05-02 21:08:11 -0400

    new index.js with async

---
+++ index copy.js
@@ -0,0 +1,99 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+inquirer.registerPrompt('loop', require('inquirer-loop')(inquirer));
+// const generatedTeamPage = require('./dist/generatedTeamPage');
+const Engineer = require('./lib/Engineer');
+
+let array = [];
+
+const info = [{
+    
+    type: 'input',
+    message: 'Enter your name',
+    name: 'name',
+}, {
+    type: 'input',
+    message: 'Enter your email',
+    name: 'email',
+}, {
+    type: 'number',
+    message: 'Enter employee ID',
+    name: 'id',
+}, {
+    type: 'list',
+    name: 'position',
+    message: 'what is your position?',
+    choices: [
+        'Manager',
+        'Engineer',
+        'Intern',
+    ]
+}, {
+    type: 'input',
+    name: 'officeNumber',
+    message: 'Please enter your Office Number',
+    when: (answers) => answers.position === 'Manager',
+}, {
+    type: 'input',
+    name: 'github',
+    message: 'Please enter your GitHub Username',
+    when: (answers) => answers.position === 'Engineer',
+}, {
+    type: 'input',
+    name: 'school',
+    message: 'Please enter your College or University name',
+    when: (answers) => answers.position === 'Intern',
+},
+];
+// console.log(info);
+
+// function writeToFile(fileName, data) {
+//     fs.appendFileSync(fileName, data, (err) => {
+//         if (err) {
+//             return console.log(err);
+//         } else {
+//             console.log('success');
+//         };
+//     });
+// };
+
+//TO DO: give user option to exit prompt. Keep looping prompt 
+function constructorToArray (data) {
+    
+    switch(data.position) {
+        case 'Engineer': 
+        array.push(new Engineer(data.name, data.id, data.email, data.github))
+        break
+    }
+}
+
+function init() {
+    inquirer
+    .prompt(info)
+    .then((data) => {
+        // console.log(data)
+        var constructorArray = constructorToArray(data)
+        console.log(constructorArray);
+        // writeToFile('index.html', generatedTeamPage(data));
+    });
+};
+init();
+
+
+// const employee = new Employee();
+
+// employee.getRole();
+
+
+
+
+
+
+
+
+
+//will need three classes: manager, engineer, intern
+//will all have name, id, email address
+//all will have methods like getId, getName, getEmail, getRole
+//all have 1 specific information unique to each
+//one class is employee class that the other three classes extend the employee class
--- index.js
+++ index.js
@@ -1,81 +1,112 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const { inherits } = require('util');
+// const { inherits } = require('util');
+// inquirer.registerPrompt('loop', require('inquirer-loop')(inquirer));
 // const generatedTeamPage = require('./dist/generatedTeamPage');
 const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
-let array = [];
+array = [];
+const collectInputs = async(inputs = []) => {
+    const prompts = [
+        {
+            type: 'input',
+            message: 'Enter your name',
+            name: 'name',
+        }, {
+            type: 'input',
+            message: 'Enter your email',
+            name: 'email',
+        }, {
+            type: 'number',
+            message: 'Enter employee ID',
+            name: 'id',
+        }, {
+            type: 'list',
+            name: 'position',
+            message: 'what is your position?',
+            choices: [
+                'Manager',
+                'Engineer',
+                'Intern',
+            ]
+        }, {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Please enter your Office Number',
+            when: (answers) => answers.position === 'Manager',
+        }, {
+            type: 'input',
+            name: 'github',
+            message: 'Please enter your GitHub Username',
+            when: (answers) => answers.position === 'Engineer',
+        }, {
+            type: 'input',
+            name: 'school',
+            message: 'Please enter your College or University name',
+            when: (answers) => answers.position === 'Intern',
+        }, {
+                type:'confirm',
+                name:'again',
+                message:'Add another employee?',
+                default: true,
+            },
+        ];
 
-const info = [{
-    type: 'input',
-    message: 'Enter your name',
-    name: 'name',
-}, {
-    type: 'input',
-    message: 'Enter your email',
-    name: 'email',
-}, {
-    type: 'number',
-    message: 'Enter employee ID',
-    name: 'id',
-}, {
-    type: 'list',
-    name: 'position',
-    message: 'what is your position?',
-    choices: [
-        'Manager',
-        'Engineer',
-        'Intern',
-    ]
-}, {
-    type: 'input',
-    name: 'officeNumber',
-    message: 'Please enter your Office Number',
-    when: (answers) => answers.position === 'Manager',
-}, {
-    type: 'input',
-    name: 'github',
-    message: 'Please enter your GitHub Username',
-    when: (answers) => answers.position === 'Engineer',
-}, {
-    type: 'input',
-    name: 'school',
-    message: 'Please enter your College or University name',
-    when: (answers) => answers.position === 'Intern',
-},
-];
-// console.log(info);
+    const {again, ...answers} = await inquirer.prompt(prompts);
+    const newInputs = [...inputs, answers];
+    return again ? collectInputs(newInputs): newInputs;
+    
+};
 
-// function writeToFile(fileName, data) {
-//     fs.appendFileSync(fileName, data, (err) => {
-//         if (err) {
-//             return console.log(err);
-//         } else {
-//             console.log('success');
-//         };
-//     });
-// };
+const main = async() => {
+    const inputs = await collectInputs();
+    console.log(inputs);
+    // init();
+    constructorToArray(inputs);
+};
+main();
+
+
+
 
 //TO DO: give user option to exit prompt. Keep looping prompt 
 function constructorToArray (data) {
-    
+    console.log(data)
     switch(data.position) {
         case 'Engineer': 
         array.push(new Engineer(data.name, data.id, data.email, data.github))
-        break
-    }
-}
-
-function init() {
-    inquirer
-    .prompt(info)
-    .then((data) => {
-        // console.log(data)
-        var constructorArray = constructorToArray(data)
-        console.log(constructorArray);
-        // writeToFile('index.html', generatedTeamPage(data));
-    });
+        break;
+        case 'Manager':
+        array.push(new Manager(data.name, data.id, data.email, data.officeNumber))
+        break;
+        case 'Intern':
+        array.push(new Intern(data.name, data.id, data.email, data.school))
+        break;
+    }; 
+    console.log(array);
+    // init(array);
 };
-init();
+
+// function init() {
+//     inquirer
+//     .prompt(array)
+//     .then((data) => {
+//         // console.log(data)
+//         var constructorArray = constructorToArray(data)
+//         console.log(constructorArray);
+//         // writeToFile('index.html', generatedTeamPage(data));
+//     });
+// };
+
+
+
+
+
+
+
 
 
 // const employee = new Employee();
